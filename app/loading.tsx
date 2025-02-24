345eb6194279268378069d38c6478b0a072730f6
Jack Clackett <jack@noquarter.co>
2025-02-24 12:37:42 +0100

    loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,3 @@
-import { CardContent } from "@/components/ui/card"
-
-import { Card } from "@/components/ui/card"
-import { LoaderIcon } from "lucide-react"
-
 export default function Page() {
-  return (
-    <Card>
-      <CardContent className="flex items-center justify-center gap-4 pt-6">
-        <span className="text-muted-foreground">Analyzing</span>
-        <LoaderIcon className="animate-spin text-muted-foreground" />
-      </CardContent>
-    </Card>
-  )
+  return null
 }
