7b76bbf2c1b32a7e61cec361c621ddd7dd520a0d
Jack Clackett <jack@noquarter.co>
2025-02-24 17:37:39 +0100

    clean up

--- app/[repo]/page.tsx
+++ app/[repo]/page.tsx
@@ -1,6 +1,5 @@
 import { StatsDisplay } from "@/components/stats-display"
-import { RepoData, analyzeRepo, getRepoKey } from "@/lib/analyze-repo"
-import { redis } from "@/lib/upstash"
+import { analyzeRepo } from "@/lib/analyze-repo"
 import { Metadata } from "next"
 
 const standardMetadata: Metadata = {
@@ -24,24 +23,10 @@ export async function generateMetadata({ params }: { params: Promise<{ repo?: st
 
   const decodedRepoUrl = decodeURIComponent(repoUrl)
 
-  const key = getRepoKey(decodedRepoUrl)
+  const result = await analyzeRepo(decodedRepoUrl)
+  if (!result.success) return standardMetadata
 
-  let result: RepoData | null = null
-  if (key) {
-    const cachedResult = await redis.hgetall<RepoData>(key)
-    if (cachedResult?.info.updatedAt && cachedResult.info.updatedAt > Date.now() - 1000 * 60 * 60) {
-      result = cachedResult
-    } else {
-      const res = await analyzeRepo(decodedRepoUrl)
-      if (!res.success) return standardMetadata
-      result = res.data
-    }
-  } else {
-    const res = await analyzeRepo(decodedRepoUrl)
-    if (!res.success) return standardMetadata
-    result = res.data
-  }
-  const title = result ? `Stats for ${result.info.owner}/${result.info.repo}` : "Rate My Next"
+  const title = result.data ? `Stats for ${result.data.info.owner}/${result.data.info.repo}` : "Rate My Next"
   return {
     title,
     description: `View the Next.js stats for ${decodedRepoUrl}`,
@@ -60,14 +45,6 @@ export default async function Page({ params }: { params: Promise<{ repo?: string
   const repoUrl = (await params).repo
   if (!repoUrl) return null
   const decodedRepoUrl = decodeURIComponent(repoUrl)
-
-  const key = getRepoKey(decodedRepoUrl)
-  if (!key) return <div className="rounded-lg bg-destructive px-6 py-4 text-sm text-white shadow-main">Invalid Repo URL</div>
-
-  const cachedResult = await redis.hgetall<RepoData>(key)
-  if (cachedResult?.info.updatedAt && cachedResult.info.updatedAt > Date.now() - 1000 * 60 * 60) {
-    return <StatsDisplay data={cachedResult} />
-  }
 
   const result = await analyzeRepo(decodedRepoUrl)
 
