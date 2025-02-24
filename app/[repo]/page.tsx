87722c57139f9c39caecc80435cb539264ac523f
Jack Clackett <jack@noquarter.co>
2025-02-24 12:35:41 +0100

    more cache

--- app/[repo]/page.tsx
+++ app/[repo]/page.tsx
@@ -14,6 +14,21 @@ export async function generateMetadata({ params }: { params: Promise<{ repo?: st
   }
 
   const decodedRepoUrl = decodeURIComponent(repoUrl)
+
+  const key = getRepoKey(decodedRepoUrl)
+  if (!key)
+    return {
+      title: "Rate My Next",
+      description: "Rate your Next.js repository.",
+    }
+  const cachedResult = await redis.hgetall<RepoData>(key)
+  if (cachedResult?.info.updatedAt && cachedResult.info.updatedAt > Date.now() - 1000 * 60 * 60) {
+    return {
+      title: `Stats for ${cachedResult.info.owner}/${cachedResult.info.repo}`,
+      description: `View the Next.js stats for ${decodedRepoUrl}`,
+    }
+  }
+
   const result = await analyzeRepo(decodedRepoUrl)
 
   const title = result.data ? `Stats for ${result.data.info.owner}/${result.data.info.repo}` : "Rate My Next"
@@ -35,8 +50,9 @@ export async function generateMetadata({ params }: { params: Promise<{ repo?: st
 export default async function Page({ params }: { params: Promise<{ repo?: string }> }) {
   const repoUrl = (await params).repo
   if (!repoUrl) return null
+  const decodedRepoUrl = decodeURIComponent(repoUrl)
 
-  const key = getRepoKey(decodeURIComponent(repoUrl))
+  const key = getRepoKey(decodedRepoUrl)
   if (!key) return <div className="rounded-lg bg-destructive px-6 py-4 text-sm text-white shadow-main">Invalid Repo URL</div>
 
   const cachedResult = await redis.hgetall<RepoData>(key)
@@ -44,7 +60,7 @@ export default async function Page({ params }: { params: Promise<{ repo?: string
     return <StatsDisplay data={cachedResult} />
   }
 
-  const result = await analyzeRepo(decodeURIComponent(repoUrl))
+  const result = await analyzeRepo(decodedRepoUrl)
 
   if (!result.success) {
     return <div className="rounded-lg bg-destructive px-6 py-4 text-sm text-white shadow-main">{result.error}</div>
